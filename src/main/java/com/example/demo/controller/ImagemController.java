e5bf5f1c4f3b201a7846df6c7c1de88868634ac0
Thais <thaiscarvalho2898@gmail.com>
2018-12-14 13:42:48 -0200

    Início da API

--- src/main/java/com/example/demo/controller/ImagemController.java
+++ src/main/java/com/example/demo/controller/ImagemController.java
@@ -7,9 +7,13 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Produto;
 import com.example.demo.services.ImagemService;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +27,25 @@ import org.springframework.web.multipart.MultipartFile;
  * @author Jaqueline
  */
 @RestController
+@RequestMapping(value = "/img")
 public class ImagemController {
         @Autowired
         ImagemService imagemService;
+        
+        @RequestMapping(method = RequestMethod.GET, value = "/{id}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<Resource> getImgProd(@PathVariable Long id) {
+
+        Resource resource;
+
+        try {
+            resource = imagemService.carregaImagemId(id);
+        } catch (NoSuchElementException | FileNotFoundException e) {
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(resource, HttpStatus.OK);
+    }    
+        
     
    @RequestMapping(value="/admin/addimgprod/{id}",method = RequestMethod.POST) 
    public ResponseEntity addImagens(
